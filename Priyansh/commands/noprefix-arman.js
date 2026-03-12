666857ee023b62f2fee13df5e57c8432fdd5811f
DEVILXID <rishikeshanand51@gmail.com>
2026-03-12 16:29:48 +0530

    Update noprefix-Rishu.js

--- Priyansh/commands/noprefix-arman.js
+++ Priyansh/commands/noprefix-arman.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("@Arman")==0 || event.body.indexOf("Arman")==0 || event.body.indexOf("ARMAN")==0 || event.body.indexOf("arman")==0) {
+	if (event.body.indexOf("@YAMRAJ")==0 || event.body.indexOf("yamraj dev")==0 || event.body.indexOf("YAMMA raj")==0 || event.body.indexOf("dev")==0) {
 		var msg = {
-				body: "𝗛𝗲 𝗶𝘀 𝗕𝗲𝘀𝘁 𝗙𝗿𝗶𝗲𝗻𝗱 𝗢𝗳 𝗠𝘆 𝗕𝗼𝘀𝘀 𝗔𝗿𝘂𝗻 \n 💞 \n 🌹𝙰𝚛𝚖𝚊𝚗 𝙼𝚊𝚕𝚒𝚔😘♥️🌹",
+				body: "𝗛𝗲 𝗶𝘀 𝗕𝗲𝘀𝘁 𝗙𝗿𝗶𝗲𝗻𝗱 𝗢𝗳 𝗠𝘆 𝗕𝗼𝘀𝘀 𝐑𝐈𝐒𝐇𝐔 𝐗 𝐑𝐈𝐒𝐇𝐈𝐊𝐄𝐒𝐇 \n 💞 \n 🌹YAMRAJ DEV😘♥️🌹",
 				attachment: fs.createReadStream(__dirname + `/noprefix/arman.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
