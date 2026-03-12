f42578439a2c0c655ca19626491bb2c444b7c755
DEVILXID <rishikeshanand51@gmail.com>
2026-03-12 16:31:31 +0530

    Update noprefix-teeth.js

--- Priyansh/commands/noprefix-teeth.js
+++ Priyansh/commands/noprefix-teeth.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("haha")==0 || event.body.indexOf("😁😁")==0 || event.body.indexOf("daant")==0 || event.body.indexOf("😁")==0) {
 		var msg = {
-				body: "𝙺𝚢𝚊 𝚊𝚙𝚔𝚎 𝚃𝚘𝚘𝚝𝚑𝚙𝚊𝚜𝚝𝚎 𝚖𝚎 𝙽𝚊𝚖𝚊𝚔 𝚑𝚊𝚒  Itna Shinny🤭🙄",
+				body: "𝙺𝚢𝚊 𝚊𝚙𝚔𝚎 𝚃𝚘𝚘𝚝𝚑𝚙𝚊𝚜𝚝𝚎 𝚖𝚎 𝙽𝚊𝚖𝚊𝚔 𝚑𝚊𝚒😁 Itna Shinny🤭🙄",
 				attachment: fs.createReadStream(__dirname + `/noprefix/teeth.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
